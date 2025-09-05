1a31e02a1121b733df4a6016ab8cc1060bcb5259
phil-lumin <philmaurer@lumin.co.uk>
2025-09-05 09:28:00 +0100

    Add Sentry error tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import * as Sentry from "@sentry/react";
 import "./index.css";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracePropagationTargets: [/^https:\/\/phil-lumin\.github\.io\/ci-graph\//],
+  release: import.meta.env.APP_VERSION,
+  sendDefaultPii: true,
+  enableLogs: true,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
